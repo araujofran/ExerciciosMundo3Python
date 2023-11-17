edcdb9d363819378476ed3e79890002586a54897
araujofran <fferreira.araujo@hotmail.com>
2023-11-17 10:33:03 -0300

    Exercicio completo Dicionario 04
    Como é bom persistir

--- dicionarioex04.py
+++ dicionarioex04.py
@@ -6,14 +6,31 @@ dados['partidas']=partidas
 c=0
 gols=[]
 dados['gols']= gols
+nºpartida=[]
+
+
 somagols=0
 
 while c <=dados['partidas']-1:
     g= int (input(f'Quantos gols na partida {c}? '))
     gols.append(g)
+    nºpartida.append(c)
     somagols+=g
     c+=1
 dados['total']=somagols
+gols_dicionario=dict(zip(nºpartida,gols)) 
+print ()
+print(20*'==')
+print('AQUI A PROVA DOS NOVE')
+print(20*'==')
+print(f'A lista do numero da posição do numero de partidas: {nºpartida}.' )
+print(20*'==')
+print(f'Aqui a lista do numero de gols/partida {gols}.')
+print(20*'==')
+print(f'Aqui transformei as listas em dicionario com sua posição/partida {gols_dicionario} .')
+print(20*'==')
+print()
+
 del dados['partidas']
 print()
 print(35*'==')
@@ -23,7 +40,12 @@ for k,v in dados.items():
     print(f'O campo {k} tem o valor {v}. ')
 print()
 print(35*'==')
-print (f'O jogador {dados["nome"]} jogou {partidas} partidas e {somagols} gols. ')
+print (f'O jogador {dados["nome"]} jogou {partidas} partidas e fez {somagols} gols. ')
+print()
+
+for k,v in gols_dicionario.items():
+    print (f'=> Na partida [0{k}] foram {v} gols...')
+
 print()
 
     
